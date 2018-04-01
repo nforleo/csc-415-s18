82a7f6b63120f7d0d9231d7ff780ee0a7c6961cf
Marco <malvarezvg@gmail.com>
2018-04-01 01:53:14 -0400

    Update myjob.sh

--- pa-2/myjob.sh
+++ pa-2/myjob.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #
-#SBATCH --job-name=filtering-images
+#SBATCH --job-name=conv2d
 #SBATCH --error=error.txt
 #SBATCH --output=output.txt
 #
+## number of cores on the compute node
+#SBATCH --cpus-per-task=40
 #SBATCH --ntasks=1
-#SBATCH --time=10:00
-#SBATCH --mem-per-cpu=512mb
 #
+#SBATCH --time=10:00
+#
+# necessary for running the AVX2 instructions
 #SBATCH --constraint=gpu
-
+#
+module load LibTIFF
 srun ./prog images/square.tif out.tif filters/gaussian_blur.txt seq 2
